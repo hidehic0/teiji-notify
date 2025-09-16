be2168134092fc651d8a4e934fe05747aaea6b75
hidehic0 <hidehico.ooba@gmail.com>
2025-09-16 20:57:33 +0900

    update(parse_time): エラーメッセージにフォーマットの情報を追加

--- src/parse_time.rs
+++ src/parse_time.rs
@@ -3,6 +3,6 @@ use chrono::NaiveTime;
 pub fn parse_time(time: String) -> Result<NaiveTime, String> {
     match NaiveTime::parse_from_str(&time, "%H:%M") {
         Ok(time) => Ok(time),
-        Err(_) => Err(format!("Failed to parse time")),
+        Err(_) => Err(format!("Failed to parse time, format is %H:%M")),
     }
 }
